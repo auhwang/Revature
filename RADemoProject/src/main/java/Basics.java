f55ad6f40188a93c8ae54f23c57abd01cb197f0f
Unknown <auhwang@ucsd.edu>
2018-12-31 17:08:28 -0800

    google place search ex

--- RADemoProject/src/main/java/Basics.java
+++ RADemoProject/src/main/java/Basics.java
@@ -1,10 +1,13 @@
-import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 public class Basics {
 
 	public static void main(String[] args) {
 		// BaseURL or Host
-		RestAssured.baseURI = "http://maps.googleapis.com";
+		RestAssured.baseURI = "https://maps.googleapis.com";
 		
 		/*
 		 * given -> when -> then -> extract
@@ -13,15 +16,19 @@ public class Basics {
 		 * 	- then - assertions to make sure we are getting the correct response
 		 *
 		 */
-		
+			
 		given().
 			param("location", "-33.8670522,151.1957362").
 			param("radius","500").
-			param("Key","AIzaSyB7QoZPYfMVkc8TgCIV3i6HZWZCR-X6c34").
+			param("key","AIzaSyDTp_ZCt2WjL9WvKOY1nf50sXWuYAAnzuI").
 		when().
-			get("maps/api/place/nearbysearch/json").
+			get("/maps/api/place/nearbysearch/json").
 		then().
-			assertThat().statusCode(200);
+			assertThat().statusCode(200)
+		.and().
+			contentType(ContentType.JSON)
+		.and().
+			body("results[0].name",equalTo("Sydney"));
 
 	}
 
